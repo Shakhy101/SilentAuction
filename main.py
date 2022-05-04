606c11156e20711aaac3d495273a5c25b4d60b14
shakhard <david.shakhar@techlib.cz>
2022-05-04 08:41:05 +0200

    testing GPG

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def find_winning_bidder(all_bids):
             winner = bidder
     print(f"The highest bid was {highest_bid} and the winner is {winner}.")
 
+# pokus
 while not bidding_ended:
     name = input("What is your name? ")
     price = int(input("What is your bid: $"))
